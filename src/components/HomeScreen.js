b181cf8f1aece99ed2258de45e0c446355e38901
amodh.pandey64 <amodh.pandey64@gmail.com>
2022-07-30 16:47:03 +0530

    Save 1

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/LoginUi.css";
-import logo from './../logo.png';
+import logo from "./../logo.png";
 
 import {
   BrowserRouter as Router,
@@ -11,35 +11,70 @@ import {
   Redirect,
   useHistory,
   useLocation,
+  useNavigate
 } from "react-router-dom";
 import { fontSize } from "@mui/system";
+import axios from 'axios'
 
 function Login() {
   const history = useHistory();
+  const navigate=useNavigate()
 
-  const assetsPage = () => {
-    history.push("/assets");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const assetsPage = (username, password) => {
+
+    const params = JSON.stringify({
+      username: username,
+
+      password: password,
+    });
+    axios
+      .post("http://192.168.1.7:3000/login", params, {
+        headers: {
+          "content-type": "application/json",
+        },
+      })
+      .then(function (response) {
+        console.log(response.data);
+        console.log('Next Page ')
+        history.push("/assets");
+
+     
+      })
+
+      .catch(function (error) {
+        console.log(error, "Error Password");
+        alert("Oops! Wrong Password or Username!");
+      });
   };
   return (
     // <form>
 
-    <div class="main-login">
+    <div className="main-login">
       <div className="logo">
-          <img src={logo} alt="Logo"
-          style={{width:250}} />
-        </div>
-      <div
-        class="form-container">
-        <form class="register-form">
+        <img src={logo} alt="Logo" style={{ width: 250 }} />
+      </div>
+      <div className="form-container">
+        <form className="register-form">
           {/* <div class="success-message">Success! Thank you for registering</div> */}
           {/* <label>Email address</label> */}
-          <input type="email" class="form-field" placeholder="Enter email" />
+          <input
+            type="text"
+            className="form-field"
+            placeholder="Enter username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
           {/* <span id="first-name-error">Please enter an email address</span> */}
           {/* <label>Password</label> */}
           <input
             type="password"
-            class="form-field"
+            className="form-field"
             placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <div className="custom-control custom-checkbox">
             <input
@@ -47,12 +82,15 @@ function Login() {
               className="custom-control-input"
               id="customCheck1"
             />
-            <p style={{fontSize:15}}>Remember me</p>
+            <p style={{ fontSize: 15 }}>Remember me</p>
           </div>
           {/* <span id="email-error">Please enter an password</span> */}
-          <button  
-          onClick={assetsPage} class="form-field" type="submit"
-          style={{background: "#4caf50",color: "white", cursor: "pointer"}}>
+          <button
+            onClick={()=>assetsPage(username, password)}
+            className="form-field"
+            type="button"
+            style={{ background: "#4caf50", color: "white", cursor: "pointer" }}
+          >
             Submit
           </button>
         </form>
